acba215e8938623905397324eb5941840d70b02c
Mohammed Arshad <arshadcheruputhore@gmail.com>
2025-06-11 13:21:26 +0530

    api base url added

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 // services/api.js
 import axios from 'axios';
-const BASE_URL = 'http://localhost:5000/api/menus';
+const BASE_URL = `${import.meta.env.VITE_API_BASE_URL}/api/menus`;
 
 export const getMenus = () => axios.get(BASE_URL);
 export const getMenu = (id) => axios.get(`${BASE_URL}/${id}`);
